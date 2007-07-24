bf9872930d632976ed1a92260cb67d0ca8ce0c03
kiba <kiba@dell.linuxdev.us.dell.com>
2007-07-24 20:33:09 +0000

    make player.png transparent

--- trunk/lib/controller.rb
+++ trunk/lib/controller.rb
@@ -14,7 +14,7 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#You can contact the author at wikipediankiba@gmail.com
+#Youcan contact the author at wikipediankiba@gmail.com
 
 class Controller
   def initialize data
