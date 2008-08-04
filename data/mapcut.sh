f41f9f71170ac0512e08841d7cb0a19003d3ade1
kiba <kiba@dell.linuxdev.us.dell.com>
2008-08-04 12:58:59 -0400

    add new shell script

---
+++ data/mapcut.sh
@@ -0,0 +1,9 @@
+convert Eta_Carinae_Nebula_1.jpg -crop 800x600+0+0 nebula1.png &&
+convert Eta_Carinae_Nebula_1.jpg -crop 800x600+800+0 nebula2.png &&
+convert Eta_Carinae_Nebula_1.jpg -crop 800x600+1600+0 nebula3.png &&
+convert Eta_Carinae_Nebula_1.jpg -crop 800x600+0+600 nebula4.png &&
+convert Eta_Carinae_Nebula_1.jpg -crop 800x600+800+600 nebula5.png &&
+convert Eta_Carinae_Nebula_1.jpg -crop 800x600+1600+600 nebula6.png &&
+convert Eta_Carinae_Nebula_1.jpg -crop 800x600+0+1200 nebula7.png &&
+convert Eta_Carinae_Nebula_1.jpg -crop 800x600+800+1200 nebula8.png &&
+convert Eta_Carinae_Nebula_1.jpg -crop 800x600+1600+1200 nebula9.png
