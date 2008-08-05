897f63a2fc974265c31dbc1c67ac064b917e1478
kiba <kiba@dell.linuxdev.us.dell.com>
2008-08-05 08:43:51 -0400

    add direction variable

--- lib/player.rb
+++ lib/player.rb
@@ -17,7 +17,7 @@
 #You can contact the author at wikipediankiba@gmail.com
 
 class Player < Character
-  attr_accessor :speed
+  attr_accessor :speed , :direction
   IMAGE_PATH = "data/game/player.png"
   def initialize engine
     super(IMAGE_PATH,engine)
