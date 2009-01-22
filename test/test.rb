8f46a820ad29303cf57a645019bcafdee70a9f8d
kiba <wikipediankiba@gmail.com>
2009-01-22 05:55:38 -0800

    add unit test file

---
+++ test/test.rb
@@ -0,0 +1,15 @@
+
+
+#library code
+require"rubygems"
+require"rubygame"
+require"rbgooey"
+require"yaml"
+require"test/unit"
+
+#Application code
+%w{
+  mapeditor.rb
+}.each{|lib| require lib}
+
+#Test code
