2d4de35eb4e94f7c3fc1678ff7580d151988d830
kiba <wikipediankiba@gmail.com>
2009-02-16 06:01:51 -0800

    add another unit test

--- test/test_changer.rb
+++ test/test_changer.rb
@@ -11,4 +11,16 @@ class Test_Changer < Test::Unit::TestCase
     @editmap.edit()
     assert @mapfiles.map[0] == 1
   end
+  def test_end_of_list
+    change(7)
+    @editmap.edit()
+    assert @mapfiles.map[0] == 0
+    assert @mapfiles.characters[0] == 0
+    assert @mapfiles.items[0] == 0
+  end
+  def change num
+    num.times do
+      @editmap.change()
+    end
+  end
 end
