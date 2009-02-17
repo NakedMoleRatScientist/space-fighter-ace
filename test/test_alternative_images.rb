914b25a2fbced7b38c2f64ba3a684faae63a5aa2
kiba <wikipediankiba@gmail.com>
2009-02-16 21:48:50 -0800

    add a last unit test"

--- test/test_alternative_images.rb
+++ test/test_alternative_images.rb
@@ -11,12 +11,23 @@ class Test_Alternative_Images < Test::Unit::TestCase
     @editmap.edit()
     assert @mapfiles.map[0] == 1
   end
-  def test_end_of_list
+  def test_character_end
     change(5)
     @editmap.edit()
     assert @mapfiles.map[0] == 0
     assert @mapfiles.characters[0] == 3
     assert @mapfiles.items[0] == 0
+  end
+  def test_items
+   change(6)
+   @editmap.edit()
+   assert @mapfiles.items[0] == 1
+   change(1)
+   @editmap.edit()
+   assert @mapfiles.items[0] == 2
+   change(1)
+   @editmap.edit()
+   assert @mapfiles.items[0] == 0
   end
   def change num
     num.times do
