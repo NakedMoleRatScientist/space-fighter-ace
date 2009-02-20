8ec1139bb0e01aef10ea9f209f6f6ede1fcfabe3
kiba <wikipediankiba@gmail.com>
2009-02-19 20:17:30 -0800

    add a test file

---
+++ test/testgame.rb
@@ -0,0 +1,17 @@
+
+#library code
+require"rubygems"
+require"rubygame"
+require"rbgooey"
+require"yaml"
+require"test/unit"
+
+#Application code
+%w{
+  engine.rb
+}.each{|lib| require lib}
+
+include Rubygame
+TTF.setup
+#test code
+require"test/test_setup_game_engine.rb"
