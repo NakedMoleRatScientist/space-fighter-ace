559912e63b565c8c80dff0b25abc93db26f76e15
kiba <wikipediankiba@gmail.com>
2009-02-19 20:24:44 -0800

    add new assertions

--- test/test_setup_game_engine.rb
+++ test/test_setup_game_engine.rb
@@ -5,9 +5,11 @@ class Test_Setup_Game_Engine < Test::Unit::TestCase
   end
   def test_inital
     assert @game.mapfiles.name == nil
+    assert @game.mapengine == nil
   end
   def test_setup_mapfiles
     @game.setmapfile("data/maps/blank.map")
     assert @game.mapfiles.name == "data/maps/blank.map"
+    assert @game.mapengine.class == MapEngine
   end
 end
