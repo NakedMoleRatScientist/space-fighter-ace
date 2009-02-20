6585ae331c7a1e2fc14dfe259ef46869704037d0
kiba <wikipediankiba@gmail.com>
2009-02-19 20:23:03 -0800

    add class for successful setup

--- test/test_setup_game_engine.rb
+++ test/test_setup_game_engine.rb
@@ -6,4 +6,8 @@ class Test_Setup_Game_Engine < Test::Unit::TestCase
   def test_inital
     assert @game.mapfiles.name == nil
   end
+  def test_setup_mapfiles
+    @game.setmapfile("data/maps/blank.map")
+    assert @game.mapfiles.name == "data/maps/blank.map"
+  end
 end
