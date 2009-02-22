08d580f8ffc4915b1b53684c1b244456bca6a338
kiba <wikipediankiba@gmail.com>
2009-02-21 19:20:46 -0800

    add more test

--- test/test_setup_game_engine.rb
+++ test/test_setup_game_engine.rb
@@ -12,4 +12,10 @@ class Test_Setup_Game_Engine < Test::Unit::TestCase
     assert @game.mapfiles.name == "data/maps/blank.map"
     assert @game.mapengine.class == MapEngine
   end
+  def test_setup_images
+    @game.setmapfile("data/maps/blank.map")
+    assert @game.mapengine.terrian[0].name == "data/placeholder.png"
+    @game.setimages("data/images.yml")
+    assert @game.mapengine.terrian[0].name == "data/nebula1.png"
+  end
 end
