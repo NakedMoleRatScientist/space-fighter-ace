9265f6eec9c1ac78934d2029de1270665b9fdb87
kiba <wikipediankiba@gmail.com>
2009-02-25 06:09:42 -0800

    add another unit test

--- test/test_setup_camera.rb
+++ test/test_setup_camera.rb
@@ -8,6 +8,13 @@ class Test_Map_Camera < Test::Unit::TestCase
     test = @game.compute()
     assert test == false
   end
+  def test_setup_following_and_still_fail
+    @game.setchar(CharType.new(@game))
+    @game.setfollow("Player")
+    test = @game.compute()
+    assert @game.camera.following.class == Player
+    assert test == false
+  end
   def test_which_is_computable?
     assert @game.camera.characters == nil
     @game.setchar(CharType.new(@game))
