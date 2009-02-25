431f280d3c2aab0135312faead7023ec1a0a476b
kiba <wikipediankiba@gmail.com>
2009-02-24 20:58:14 -0800

    add a unit test

--- test/test_setup_camera.rb
+++ test/test_setup_camera.rb
@@ -8,4 +8,10 @@ class Test_Map_Camera < Test::Unit::TestCase
     test = @game.compute()
     assert test == false
   end
+  def test_which_is_computable?
+    assert @game.camera.characters.class == nil
+    @game.setchar(CharType.new(@game))
+    assert @game.camera.characters.class == Character_Trackers
+	
+  end
 end
