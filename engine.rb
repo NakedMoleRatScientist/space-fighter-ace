bf3ccfbfe81eab84b044fe7acd442df6c180fbd9
kiba <wikipediankiba@gmail.com>
2009-02-24 21:00:20 -0800

    move the camera file to a different directory

--- engine.rb
+++ engine.rb
@@ -13,13 +13,13 @@
   lib/map/mapdata.rb
   lib/map/mapfiles.rb
   lib/map/maptile.rb
-  lib/map/camera.rb
   lib/map/mapcalculator.rb
   lib/map/mapterrain.rb
   lib/map/mapengine.rb
 }.each{|lib| require lib}
 
 %w{
+  lib/engine/camera.rb
   lib/engine/timer.rb
   lib/engine/engineinfo.rb
   lib/engine/gameengine.rb
