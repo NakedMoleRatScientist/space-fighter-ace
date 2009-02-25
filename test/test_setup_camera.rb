31c2ccde1688d2bed530b9bfaaa3aa82af76794b
kiba <wikipediankiba@gmail.com>
2009-02-24 20:38:58 -0800

    change the requirement around

--- test/test_setup_camera.rb
+++ test/test_setup_camera.rb
@@ -2,10 +2,9 @@
 class Test_Map_Camera < Test::Unit::TestCase
   def setup
     @game = GameEngine.new()
-    @game.setmapfile("test/blank.map")
   end
   def test_initial
-    assert @game.mapengine.camera.class == Camera
+    assert @game.camera.class == Camera
     test = @game.compute()
     assert test == false
   end
