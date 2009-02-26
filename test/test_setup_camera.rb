aeb8e9c54b389785f34d04ac6742c098de1d3536
kiba <wikipediankiba@gmail.com>
2009-02-25 19:43:23 -0800

    add unit test

--- test/test_setup_camera.rb
+++ test/test_setup_camera.rb
@@ -12,9 +12,14 @@ class Test_Map_Camera < Test::Unit::TestCase
     @game.setchar(CharType.new(@game))
     @game.characters_tracker.addchar(2,100,100)
     @game.setfollow("Player")
-    test = @game.compute()
     assert @game.camera.following.class == Player
+    test = @game.camera.compute()
     assert test == true
+  end
+  def test_will_not_activate_without_following_even_if_everything_else_is_set
+    @game.setmapfile("test/blank.map")
+    test = @game.camera.compute()
+    assert test == false
   end
   def test_which_is_computable?
     assert @game.camera.characters == nil
