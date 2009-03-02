8c4e8a17bdf33220bdc1b7531590e7f01d488c54
kiba <wikipediankiba@gmail.com>
2009-03-01 22:00:14 -0800

    add additional assertions

--- test/test_camera_move.rb
+++ test/test_camera_move.rb
@@ -4,12 +4,14 @@ class Test_Camera_Move < Test::Unit::TestCase
     @game = GameEngine.new()
     @game.setchar(CharType.new(@game))
     @game.setmapfile("test/blank.map")
-    @game.characters_tracker.addchar(2,100,100)
+    @game.characters_tracker.addchar(2,300,100)
     @game.setfollow("Player")
   end
   def test_move_once_only
-    @game.following.rect.x = 100
     @game.following.right()
-    assert @game.following.rect.x == 150
+    @game.compute()
+    assert @game.following.rect.x == 350
+    assert @game.camera.width == 0
+    assert @game.camera.height == 0
   end
 end
