3976c4936779e525b446ebf3f923ef7ca8e134bf
kiba <wikipediankiba@gmail.com>
2009-03-01 21:37:46 -0800

    fix crash bugs

--- test/test_camera_move.rb
+++ test/test_camera_move.rb
@@ -1,14 +1,14 @@
 
 class Test_Camera_Move < Test::Unit::TestCase
   def setup
-    @camera = Camera.new()
+    @game = GameEngine.new()
     @game.setchar(CharType.new(@game))
     @game.characters_tracker.addchar(2,100,100)
     @game.setfollow("Player")
   end
   def test_move_once_only
     @game.following.rect.x = 100
-    @game.following.move_right()
+    @game.following.right()
     assert @game.following.rect.x == 100
   end
 end
