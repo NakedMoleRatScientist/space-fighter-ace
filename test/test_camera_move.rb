c94bc10675638ed6881d5bda701cc25d5ac53f36
kiba <wikipediankiba@gmail.com>
2009-03-01 21:34:01 -0800

    add a unit test

--- test/test_camera_move.rb
+++ test/test_camera_move.rb
@@ -1,5 +1,14 @@
 
 class Test_Camera_Move
   def setup
+    @camera = Camera.new()
+    @game.setchar(CharType.new(@game))
+    @game.characters_tracker.addchar(2,100,100)
+    @game.setfollow("Player")
+  end
+  def test_move_once_only
+    @game.following.rect.x = 100
+    @game.following.move_right()
+    assert @game.following.rect.x == 100
   end
 end
