62f63ce4dfcf3f64daac713871ac7bb72a7f44d1
kiba <wikipediankiba@gmail.com>
2009-03-09 18:26:35 -0700

    add a unit test

--- test/test_space_object_movement.rb
+++ test/test_space_object_movement.rb
@@ -7,6 +7,11 @@ class Test_Space_Object_Movement < Test::Unit::TestCase
   end
   def test_initial
     assert @player.speed == 0
-    assert @player.movement == 0
+    assert @player.movement == 0.0
+  end
+  def test_set_velocity
+    @player.set_velocity(4)
+    assert @player.speed == 4
+    assert @player.movement == 0.4
   end
 end
