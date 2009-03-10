c034bb95f587d941e68855501c77703a0d618862
kiba <wikipediankiba@gmail.com>
2009-03-09 18:17:22 -0700

    add a unit test file

---
+++ test/test_space_object_movement.rb
@@ -0,0 +1,11 @@
+
+
+class Test_Space_Object_Movement < Test::Unit::TestCase
+  def setup
+    @engine = GameEngine.new()
+    @player = Player.new(@engine)
+  end
+  def test_initial
+    assert @player.speed == 0
+  end
+end
