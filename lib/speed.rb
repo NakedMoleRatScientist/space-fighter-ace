ba191bc799b253d84a845a6105deb597eecdb0fc
kiba <wikipediankiba@gmail.com>
2009-03-10 19:53:21 -0700

    add everything from spaceobject class

--- lib/speed.rb
+++ lib/speed.rb
@@ -9,4 +9,24 @@ class Speed
     @predicted_position = 0
     @times = 0
   end
+  def move
+    if @times == 0
+      @predicted_position = @rect.x + @speed
+    end
+    @position += @movement
+    @addup += @movement
+    @times += 1
+    if @addup >= 1
+      @addup = 0.0
+      @rect.x += 1
+    end
+    if @times == 10
+      @rect.x = @predicted_position
+      @times = 0
+    end
+  end
+  def set_speed n
+    @speed = n
+    @movement = @speed / 10.0
+  end
 end
