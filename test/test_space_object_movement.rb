2e05e41d5f8b86be6a1e1db4e5f307594751a0c4
kiba <wikipediankiba@gmail.com>
2009-03-10 19:54:59 -0700

    rename everything in preparation for class spilt in spaceobject class

--- test/test_space_object_movement.rb
+++ test/test_space_object_movement.rb
@@ -6,38 +6,38 @@ class Test_Space_Object_Movement < Test::Unit::TestCase
     @player = Player.new(@engine)
   end
   def test_initial
-    assert @player.speed == 0
-    assert @player.movement == 0.0
+    assert @player.speed.speed == 0
+    assert @player.speed.movement == 0.0
   end
   def test_set_velocity
     @player.set_velocity(4)
-    assert @player.speed == 4
-    assert @player.movement == 0.4
-    assert @player.position == 0
+    assert @player.speed.speed == 4
+    assert @player.speed.movement == 0.4
+    assert @player.speed.position == 0
     assert @player.rect.x == 0
   end
   def test_set_velocity_and_move
     @player.set_velocity(4)
-    assert @player.speed == 4
-    assert @player.movement == 0.4
-    assert @player.position == 0.0
+    assert @player.speed.speed == 4
+    assert @player.speed.movement == 0.4
+    assert @player.speed.position == 0.0
     assert @player.rect.x == 0
     move(10)
-    assert @player.position.class == Float
-    assert @player.position.round == 4
+    assert @player.speed.position.class == Float
+    assert @player.speed.position.round == 4
     assert @player.rect.x == 4
   end
   def test_set_velocity_part_way
     @player.set_velocity(4)
-    assert @player.speed == 4
-    assert @player.movement == 0.4
+    assert @player.speed.speed == 4
+    assert @player.speed.movement == 0.4
     assert @player.rect.x == 0
     move(3)
-    assert @player.position.round == 1
+    assert @player.speed.position.round == 1
     assert @player.rect.x == 1
   end
   def test_time_test
-    @player.set_velocity(4)
+    @player.set_speed(4)
     state = false
     timer = Timer.new(1.1) {
       state = true
