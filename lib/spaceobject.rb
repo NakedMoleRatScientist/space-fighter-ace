6281495dfe657f83b6f45dd374d5e9f0391e778f
kiba <wikipediankiba@gmail.com>
2009-03-10 19:23:37 -0700

    whoops. forgot this one

--- lib/spaceobject.rb
+++ lib/spaceobject.rb
@@ -23,11 +23,27 @@ class SpaceObject < Character
     @speed = 0
     @movement = 0
     @position = 0.0
+    @addup = 0.0
+    @predicted_position = 0
+    @times = 0
   end
   def act
   end
   def move
+    if @times == 0
+      @predicted_position = @rect.x + @speed
+    end
     @position += @movement
+    @addup += @movement
+    @times += 1
+    if @addup >= 1
+      @addup = 0.0
+      @rect.x += 1
+    end
+    if @times == 10
+      @rect.x = @predicted_position
+      @times = 0
+    end
   end
   def set_velocity n
     @speed = n
