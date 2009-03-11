77782d2d4929fa8cc8f2d8b998116fcbce494ecf
kiba <wikipediankiba@gmail.com>
2009-03-10 19:52:08 -0700

    add attributes and stuff

--- lib/speed.rb
+++ lib/speed.rb
@@ -1,5 +1,12 @@
 
 class Speed
+  attr_reader :speed , :movement , :position
   def initialize
+    @speed = 0
+    @movement = 0
+    @position = 0.0
+    @addup = 0.0
+    @predicted_position = 0
+    @times = 0
   end
 end
