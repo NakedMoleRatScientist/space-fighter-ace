162b40fd763b355e300c455f431c88201afd85e8
kiba <wikipediankiba@gmail.com>
2009-03-11 17:25:39 -0700

    add two new methods

--- lib/regulator.rb
+++ lib/regulator.rb
@@ -3,6 +3,10 @@ class Regulator
   def initialize
     @times = 0
   end
+  def set_action_for_zero &action_zero
+  end
+  def set_action_for_final &acton_ten
+  end
   def times_equal_zero? &action
     if @times == 0
       @action.call
