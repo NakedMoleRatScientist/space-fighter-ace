0905694054438d5508cdcf4ab358182f9556979d
kiba <wikipediankiba@gmail.com>
2009-03-11 17:27:52 -0700

    add the tick method

--- lib/regulator.rb
+++ lib/regulator.rb
@@ -7,6 +7,10 @@ class Regulator
   end
   def set_action_for_final &acton_ten
   end
+  def tick
+    times_equal_zero?
+    times_equal_ten?
+  end
   def times_equal_zero?
     if @times == 0
       @action_zero.call
