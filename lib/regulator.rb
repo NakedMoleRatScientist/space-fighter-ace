61fe8e6a0ef74b3a65ab02fbeb9c7be6c96430f0
kiba <wikipediankiba@gmail.com>
2009-03-11 17:26:37 -0700

    rename stuff and eliminate unneccesary parameter

--- lib/regulator.rb
+++ lib/regulator.rb
@@ -7,14 +7,14 @@ class Regulator
   end
   def set_action_for_final &acton_ten
   end
-  def times_equal_zero? &action
+  def times_equal_zero?
     if @times == 0
-      @action.call
+      @action_zero.call
     end
   end
-  def times_equal_ten? &action
+  def times_equal_ten?
     if @times == 10
-      @action.call
+      @action_ten.call
     end
   end
 end
