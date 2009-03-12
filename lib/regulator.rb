72025e150c5e0ab5eed0251a80c11fdb81a8dcfd
kiba <wikipediankiba@gmail.com>
2009-03-11 17:29:17 -0700

    add the stuff that does thing in the middle

--- lib/regulator.rb
+++ lib/regulator.rb
@@ -7,8 +7,11 @@ class Regulator
   end
   def set_action_for_final &acton_ten
   end
+  def set_do_in_between &action_between
+  end
   def tick
     times_equal_zero?
+    @action_between.call
     times_equal_ten?
   end
   def times_equal_zero?
