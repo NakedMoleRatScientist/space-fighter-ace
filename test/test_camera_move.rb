f1d9e6b6bd178eaef5558c358bb329c48a50cd20
kiba <wikipediankiba@gmail.com>
2009-03-16 18:27:08 -0700

    add even more unit test

--- test/test_camera_move.rb
+++ test/test_camera_move.rb
@@ -36,4 +36,19 @@ class Test_Camera_Move < Test::Unit::TestCase
     assert @game.camera.width == -50
     assert @game.camera.height == 0
   end
+  def test_camera_move_with_enemies
+    @game.characters_tracker.addchar(1,400,100)
+    assert @game.following.rect.x == 300
+    @game.following.right()
+    @game.following.right()
+    @game.following.right()
+    assert @game.following.rect.x == 450
+    assert @game.camera.width == 0
+    assert @game.camera.height == 0
+    enemy = @game.characters_tracker.lookup("Enemy Fighter A",true)
+    assert enemy.rect.x == 400
+    @game.compute()
+    assert @game.following.rect.x == 350
+    assert enemy.rect.x == 300
+  end
 end
