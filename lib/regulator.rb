fa2b0ee0d1492d6174cc2f86cd29565e40a37c31
kiba <wikipediankiba@gmail.com>
2009-03-21 19:50:28 -0700

    remove all cruft"

--- lib/regulator.rb
+++ lib/regulator.rb
@@ -1,33 +1,14 @@
 
 class Regulator
   def initialize
-    @times = 0
     @timer = Array.new()
   end
   def set_action &action
+    @timer << action
   end
-  def set_action_for_zero &action_zero
-    @action_zero = action_zero
-  end
-  def set_action_for_final &action_ten
-    @action_ten = action_ten
-  end
-  def set_do_in_between &action_between
-    @action_between = action_between
-  end
-  def tick
-    times_equal_zero?
-    @action_between.call
-    times_equal_ten?
-  end
-  def times_equal_zero?
-    if @times == 0
-      @action_zero.call
-    end
-  end
-  def times_equal_ten?
-    if @times == 10
-      @action_ten.call
+  def call_all
+    @timer.each do |object|
+      object.call()
     end
   end
 end
