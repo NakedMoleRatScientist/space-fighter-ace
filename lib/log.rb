dbbce091d69e20ddff236b5b26c1639249a02c90
kiba <wikipediankiba@gmail.com>
2009-03-30 20:07:04 -0700

    change location of log

--- lib/log.rb
+++ lib/log.rb
@@ -1,7 +1,7 @@
 
 class Log
   def initialize
-    @logger = Logger.new('log.log')
+    @logger = Logger.new('log/camera.log')
   end
   def new_position x , y
     @logger.debug("new position of object being followed by camera: #{x} , #{y}")
