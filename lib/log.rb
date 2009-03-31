a71abe1a7071923ba085f67533d2510eaca7f3a6
kiba <wikipediankiba@gmail.com>
2009-03-30 20:02:08 -0700

    add a method 'new_position'

--- lib/log.rb
+++ lib/log.rb
@@ -3,4 +3,7 @@ class Log
   def initialize
     @logger = Logger.new('log.log')
   end
+  def new_position x , y
+    @logger.debug("new position of object being followed by camera: #{x} , #{y}")
+  end
 end
