297ccf2f20e4af8b3739e9b14fd7fe68cae8628f
kiba <wikipediankiba@gmail.com>
2009-04-04 18:35:41 -0700

    add a new method

--- lib/camera_log.rb
+++ lib/camera_log.rb
@@ -3,7 +3,8 @@ class Camera_Log
   def initialize
     @logger = Logger.new('log/camera.log')
   end
-  def new_position x , y
+  def new_position x , y , rect
     @logger.debug("new position of object being followed by camera: #{x} , #{y}")
+    @logger.debug("absolute location of object on the map: #{rect.x}, #{rect.y}")
   end
 end
