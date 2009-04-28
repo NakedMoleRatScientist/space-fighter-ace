131075058ebccbb08ac17c3384de087c52257d7d
kiba <wikipediankiba@gmail.com>
2009-04-28 16:10:44 -0700

    remove old stuff

--- lib/player.rb
+++ lib/player.rb
@@ -30,8 +30,6 @@ class Player < SpaceObject
     @engine.charstrack.set_control_last()
     @engine.charstrack.control.state = false
     @engine.charstrack.control.image_set()
-    @engine.charstrack.control.movement.directionchange(1)
-    @engine.charstrack.control.movement.speed = @movement.speed + 5
   end
   def up
     @rect.y -= 50
@@ -40,10 +38,10 @@ class Player < SpaceObject
     @rect.y += 50
   end
   def right
-    @speed.right()
+    @rect.x += 50
   end
   def left
-    @speed.left()
+    @rect.x -= 50
   end
   def hook_move
     hooks = {
--- lib/spaceobject.rb
+++ lib/spaceobject.rb
@@ -20,21 +20,7 @@ class SpaceObject < Character
   attr_accessor :speed , :rotation , :location
   def initialize image, engine
     super(image,engine)
-    @location = Rubygame::Ftor.new(0,-1)
-    @location.angle = 3 * Math::PI/ 2
-    @location.magnitude = 1
-    @speed = Speed.new(self)
-    @rotation = Rotation.new(image)
-    @timer = Timer.new(0.1) {
-      @speed.move()
-    } 
-    @timer.start()
   end
   def act
-    @timer.check()
-    convert_vector_to_rect()
-  end
-  def convert_vector_to_rect
-    @rect.center = @location.to_ary
   end
 end
