009eabf5891e985df4d0b301451bfd14a85299e6
kiba <wikipediankiba@gmail.com>
2009-04-30 16:22:59 -0700

    add an image to load

--- test/test_space_object_movement.rb
+++ test/test_space_object_movement.rb
@@ -5,7 +5,7 @@
 class Test_Space_Object_Movement < Test::Unit::TestCase
   context "Simulate 10 seconds worth of movement" do
     setup do
-      @object = SpaceObject.new()
+      @object = SpaceObject.new("data/game/enemy.png")
       @object.simulate()
     end
     should "should have 10 objects.." do
