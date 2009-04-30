ad4cb1d981c99506972e1f58fd0fa20156bca2b2
kiba <wikipediankiba@gmail.com>
2009-04-30 16:09:21 -0700

    correct naming error

--- test/test_space_object_movement.rb
+++ test/test_space_object_movement.rb
@@ -5,7 +5,7 @@
 class Test_Space_Object_Movement < Test::Unit::TestCase
   context "Simulate 10 seconds worth of movement" do
     setup do
-      @object = Space_Object.new()
+      @object = SpaceObject.new()
       @object.simulate()
     end
     should "should have 10 objects.." do
